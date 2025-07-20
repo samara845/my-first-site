47e232d8d2354523aa0d823ac2b4db0811873e55
samara845 <itz.royal.004@gmail.com>
2025-07-20 21:13:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+
+
+const items = {
+            "sky force": "https://drive.google.com/file/d/1cl4syb7OXz1hqZylBCl6Mym3E6hdhzpk/view?usp=sharing",
+            "hero panti": "https://drive.google.com/file/d/1Li3SI4vWkZmPxMzu3gB63eNAxy0yfX4Q/view?usp=sharing",
+            "Emergency": "https://drive.google.com/file/d/1_XywBQmIWB37TZDkEqPZE8d9fM9jo7IX/view",
+            "Azaad": "https://drive.google.com/file/d/1rKKX21OxZXSPgUJNequsaI5-L6yT_Nhr/view",
+            "Hisaab barabar": "https://drive.google.com/file/d/1-Zx4tTi-xgCuD4H9btYGHnQlnJb-BVAf/view",
+            "Vanvaas": "https://drive.google.com/file/d/1AlpB7flNpUZX3N8UMqo-KcnTmmTy85jK/view?usp=sharing",
+            "baby jhon": "https://drive.google.com/file/d/19HzhgleV-w7GbzBhiMT__VnsnkUWu1Mx/view?usp=sharing ",
+            "Munjya": "https://drive.google.com/file/d/1Sj9nVX8y5q5dpjop4hWm_J-32xA678U7/view?usp=sharing",
+            "Marco": "https://drive.google.com/file/d/1Lf7etz7oRvv8gCgH0kkl8F5GPEtyXpPQ/view?usp=sharing",
+            "Bhul Bhulaiya 3": "https://drive.google.com/file/d/1Am_jyo4JPDty7ZQpGrej8kU6t1c9jh8V/view?usp=sharing",
+            "Vicky Vidya Ka Wo Wala Video": "https://drive.google.com/file/d/1P28iWjIuPnTs7uABVvo1OaANobCRC_7h/view?usp=sharing",
+            "Ulajh" : "https://drive.google.com/file/d/1uIPj-EP9FF2zdT20U7hjYSIy1G3ggM2N/view?usp=sharing",
+            "Phir se aayi dil ruba" : "https://drive.google.com/file/d/16AN5yMxuZyYwZ-tfvKw0SgKN7ospuPbj/view?usp=sharing",
+            "Ruslaan" : "https://drive.google.com/file/d/1rf0x0DscPKwzE5FLHX7-K2IKpv9oB7T1/view?usp=sharing",
+            "sikandar ka mukadaddar": "https://drive.google.com/file/d/18Dg_F6CC4QTT1Tykk5cMEKXm3dgWw5HO/view?usp=sharing",
+            "Yodha" : "https://drive.google.com/file/d/1lqZKvi9gBFjOOvVCpHyOO6ur0mzPCLwx/view?usp=sharing",
+            "kalki" : "https://drive.google.com/file/d/1SEmcCAQDuxf8p9cLG-fIwRU7rn9oOlai/view?usp=sharing",
+            "Mr. & Mrs. Mahi" : "https://drive.google.com/file/d/1Vh4IDgFw81LwA5nD5oZjaN4TjpYqv6Ve/view?usp=sharing",
+            "Sarfira" : "https://drive.google.com/file/d/15JaHlmyBpupZwM_9nWzMiFIjbtkX2n_4/view?usp=sharing",
+            "do patti" : "https://drive.google.com/file/d/1MIwM4gVbgUEUKJuzH-ou9bKfagNt2XQX/view?usp=sharing",
+            "devra" : "https://drive.google.com/file/d/1XdAoBtBoT4b0jPR4oTzv-FoJnXjpprR6/view?usp=sharing",
+            "Sector 36" : "https://drive.google.com/file/d/1chRXLKQhg0e4IPaRYaTWRIKlfetgy7q7/view?usp=sharing",
+            "Pushpa 2" : "",
+            "Kill" : "https://drive.google.com/file/d/1H9eGnRPD2uXa9hm6ZqTDB3HUPs3BtxP1/view?usp=sharing",
+            "amaran" : "https://drive.google.com/file/d/1dopPQ6gus4GFvEgkRe4uO6_JZW8sLFBt/view?usp=sharing",
+            "Dashmi": "https://drive.google.com/file/d/17bk0afoue7SGPFDXB4cGVFyYDJ8dJq49/view?usp=sharing",
+            "Jigra" : "https://drive.google.com/file/d/1mttSHQ5Caxy5IlbOUk6hMToH2NnkVS_W/view?usp=sharing",
+            "Khel khel me" : "https://drive.google.com/file/d/11BC32SPtEeRhwexA_FzcVon2KdRv_8_b/view?usp=sharing",
+            "Agni" : "https://drive.google.com/file/d/1GU3Gvp1KGBr8uo9CGhnxidgQUjK29sEE/view?usp=sharing",
+            "Animal" : "https://drive.google.com/file/d/1LyPut_MnM14xCMaqayp5hKP1V1D7v3oA/view?usp=sharing",
+            "12th Fail": "https://drive.google.com/file/d/1-pot1tLA7HVU7nZNa9ctmpfpK374gV1V/view?usp=sharing",
+            "Gaddar 2" : "https://drive.google.com/file/d/1q03C9PwHOqcV-dttFKTn-VnN2XNZD-uT/view?usp=sharing",
+            "Tu jhuthi mai makkar" : "https://drive.google.com/file/d/12GO6K14IvuSqiwo9GnrkOxpp2qP4LHM5/view?usp=sharing",
+            "Omg 2": "https://drive.google.com/file/d/13otDyGIGkkciaOThABwzVGk09M-pfCLT/view?usp=sharing",
+            "Jaat" : "https://drive.google.com/file/d/1dg3Dq7z3H62qeG5-j_M9w6BlII55beuY/view?usp=sharing",
+            "Chhava" : "https://drive.google.com/file/d/1GBnSOU35SCAQpkuk8RdF7hxC6nRQX9s2/view?usp=sharing",
+            "School friends 2" : "https://drive.google.com/file/d/12_7dRF7NLHrH-aLHMpWNGNMhNH6eruBn/view?usp=sharing",
+            "Panchayat Season 1" : "https://drive.google.com/file/d/1j29ktY7hT8GqnoCFsN_VhG4NiFMHUyvX/view",
+            "Panchayat Season 2":"https://drive.google.com/file/d/12j2x3GQHkrkDCYTlxs-dSzli8LEoUeA7/view",
+            "Gulak Season 1": "https://drive.google.com/file/d/1-iSbmiKFj2yHlG4lYj1TJcBk3vBcNoqe/view",
+            "Gulak Season 2": "https://drive.google.com/file/d/1ROX8E2c27HXQ18GNxIs3xgYu-s4bsgA-/view",
+            "Gualk Season 3": "https://drive.google.com/file/d/1BsnT-dLiyCtKtMODjPrFhQv-Xy5-nkQ2/view",
+            "Gulak Season 4" : "https://drive.google.com/file/d/1qfKQSyejIiApC5AgcqHf4qszKNw2wAS_/view",
+            "Bhuji &amp": "https://drive.google.com/file/d/1fKHDjJjreyYZublroatatu6sQ78lxvpG/view",
+            "The Royals" : "https://drive.google.com/file/d/1C_tXIjxPsjBwHVoglBkM_HvzI9At-3qv/view",
+            "Gyaarah Gyaarah":"https://drive.google.com/file/d/1gaBDz9DLXI3nljk0rh8Faxt4DjXuanYf/view",
+            "Criminal Justice":"https://drive.google.com/file/d/1fOOkeToj-hh8gSUjNjCUjztiUZ88w_64/view",
+            "Heart Beat":"https://drive.google.com/file/d/13naIccEy6ihEI0BH9ZX8RejEfJo30h02/view",
+            "Kanneda":"https://drive.google.com/file/d/13naIccEy6ihEI0BH9ZX8RejEfJo30h02/view",
+            "Gram Chikitsalay":"https://drive.google.com/file/d/1KWoC8-JKEgrh4Qb4fXYtvRcLNlw4vKbP/view",
+            "Miss Match Season 1":"https://drive.google.com/file/d/1-rYjLxnqD_EHPJSPnMvAPmApW2bA8H2W/view",
+            "Aashram":"https://drive.google.com/file/d/1eFKmxP38WaPYn05zLDTRc2ArOFHH-SwX/view",
+            "Thukra ke mera pyaar":"https://drive.google.com/file/d/1s0XLDIq_yJ3OMX9jhRkOCKptCYo4LKvG/view",
+            "Hustlers":"https://drive.google.com/file/d/15IjlMM2OT4xR0sgxfGq-M4L8oVpfAqnk/view",
+            "Kota factory":"https://drive.google.com/file/d/155YiqVFXqyTjUr8soj9GWQDl0EWCdj-D/view",
+            "Bichhu ka khel":"https://drive.google.com/file/d/196Kcs2JIcKsI7mHkQZY2HSQTj_Ji0tMJ/view",
+            "Gutar gu season 2":"https://drive.google.com/file/d/11jv34H8VS7a7C9m5cnYqIez4zUxnPhes/view",
+            "Taza khabar":"https://drive.google.com/file/d/1DPi4SUzFdD5mF9T_0MkzEximUVgo_4IP/view",
+            "Family man Season 1":"https://drive.google.com/file/d/1reVGuWWBOXilw4pVP4amODmdFnabZ7d_/view",
+            "Family man Season1":"https://drive.google.com/file/d/1Nk4id9lVxjitnweFmrSHvB3D7pggS_Je/view",
+            "Panchayat Season 3":"https://drive.google.com/file/d/1AEwymnUGnxzzByIRb8qYbTkivA0Xb9DE/view",
+            "Gutar gu Season 1":"https://drive.google.com/file/d/1PYAMzS8wlRJVLQ6038gry1A_T_Ppps7m/view",
+            "Bhootni":"https://drive.google.com/file/d/1jt6SpHDiJwTvzjP7YxOGQopvnIlbT5ai/view",
+            "Mere Husband ki biwi":"https://drive.google.com/file/d/1Ja69fUaKP5GSTS_qHwrNx7bqbFJJJ_LM/view",
+            "Mrs.":"https://drive.google.com/file/d/1Tmk9wfZ2Oimurz7qvLQW_ym0gbzxFK1y/view?usp=sharing",
+            "Deva":"https://drive.google.com/file/d/1UkiC4i5Nv65UFAouL6HuSu-r1rHE5Ebf/view?usp=sharing",
+            "Mehta Boys":"https://drive.google.com/file/d/1bH1zadWkMbVIRM7K3_qQKOfNdic8Euqz/view?usp=sharing",
+            "The Diplomat":"https://drive.google.com/file/d/1JRLZECv-B9zy49Qnv1bmtARBcx2OX2lu/view?usp=sharing",
+            "Be Happy":"https://drive.google.com/file/d/1bxEErkkx86mIJWbXWUya2QSBj2HXSsgL/view?usp=sharing",
+            "Crazy":"https://drive.google.com/file/d/1tqrRII42rNWfFfCWTwMmaqxt0g7np4TC/view?usp=sharing",
+            "Gram Chikitsalay":"https://drive.google.com/file/d/1KWoC8-JKEgrh4Qb4fXYtvRcLNlw4vKbP/view?usp=sharing",
+            "Sikandar":"https://drive.google.com/file/d/1z4RbtLYndAbRElbklMwn986FG_r0lmjC/view?usp=sharing",
+            "Logout":"https://drive.google.com/file/d/1xirv48JCXgkjGvoWw8RYMlJMt2HCrHfU/view?usp=sharing",
+            "Phule":"https://drive.google.com/file/d/1xywBb6s0RSmMVJep7q5fn0iSz7_NR9uW/view?usp=sharing",
+            "Raid 2":"https://drive.google.com/file/d/12nE3KXdECxcP-bUpm4sMnzItaU_8VHma/view?usp=sharing",
+            "Costao":"https://drive.google.com/file/d/17cdM_LT7pPRKVtrI1cd_19e_Cs27O_Uw/view?usp=sharing",
+            "Ground Zero":"https://drive.google.com/file/d/1Cu7f_kOVyEtA1RgUHl996eo1BDZ4G5Ku/view?usp=sharing",
+            "Chhorii2":"https://drive.google.com/file/d/1zODYV6rguLMa79ogOh1zcT2wPD09nGB9/view?usp=sharing",
+            "Hit 2":"https://drive.google.com/file/d/1zODYV6rguLMa79ogOh1zcT2wPD09nGB9/view?usp=sharing",
+            "Kesari chapter 2":"https://drive.google.com/file/d/1TPacwENGqO9Kcs4QUyrUFDal5sooLLnC/view",
+            "Badass Ravi Kumar":"https://drive.google.com/file/d/1McKv9uhgw57DX4fI4nPKnYtwk0ZPal6Z/view",
+             "Fateh":"https://drive.google.com/file/d/1-5z7PkRupHJOStvpHWLouHgFkN1EODPh/view",
+             "Zwigato":"https://drive.google.com/file/d/1sNj7_JUT0vzVex0HQA3dJLAkLkzAW6sk/view",
+             "Dono":"https://drive.google.com/file/d/1jSUhwMf-GQsrmqAa5LJRxt_iU-252oYb/view",
+             "Sam Bahadur":"https://drive.google.com/file/d/1TfMQL4A3TCZZwm5ZjrurTQ_H9qumbJTt/view",
+             "Jawan":"https://drive.google.com/file/d/1h1ppL8o4feR8NcowYyffD4rk9hR5y6ZZ/view",
+             "Dunki":"https://drive.google.com/file/d/14Y8SOKJEE-uDnfPy4cSJ0IaCiasKYhvs/view",
+             "StarFish":"https://drive.google.com/file/d/1_xxO-sZAlyIqKVxcUvLB5NObS7vuiHj1/view",
+             "Tejas":"https://drive.google.com/file/d/1Yj3MRujHnUD2yAHlzCpQtxjk-TFEkcjB/view",
+             "Tiger 3":"https://drive.google.com/file/d/1Vvxwgy8Ug_tBMnbgIOBEPP5XrI1Wl7Sz/view",
+             "Govnda naam mera":"https://drive.google.com/file/d/1A_Sq9JpKXq29Fen421qblKehTWV4ZLUp/view",
+             "Uunchai":"https://drive.google.com/file/d/1YPvDlV8RLSe1fZvezJ5GCXqKzwWEwnUX/view",
+             "Mere Desh ki Dharti":"https://drive.google.com/file/d/1J9dg58h95-OtEO_kdV7ONuQuJyR0z0HL/view",
+             "Freddy":"https://drive.google.com/file/d/1R5_UmIP8d8EPVITqyrD_78XHPZb8v_ep/view",
+             "Jogi":"https://drive.google.com/file/d/1vtQ0st3RzzGQiZB4fsON5Vw-wRF9V_5l/view",
+             "GoodBye":"https://drive.google.com/file/d/1fHUogiWT5DsB9-sm_YKBE45uWBXCTZxC/view",
+             "Ved":"https://drive.google.com/file/d/1hiZuM592bKQMQw5EMWw9bdSK-1wDsUAp/view",
+             "Qala":"https://drive.google.com/file/d/1iRisf5ozyLwArlTgG1cZjy35GQcI94oS/view",
+             "Monika oh my darling":"https://drive.google.com/file/d/1Pk0EdkymfEbI-ocv4Ab7zNNgjugS4qme/view",
+             "Middle class love":"https://drive.google.com/file/d/1z2UVXEHnp6MSeEqtMUBKbIv_2hx08cG1/view",
+             "Hero Panti":"https://drive.google.com/file/d/1Li3SI4vWkZmPxMzu3gB63eNAxy0yfX4Q/view",
+             "Holiday":"https://drive.google.com/file/d/1Kwiy_ZAFdRhGjvoW0KrD1fQPDVtD0oop/view",
+             "Main Tera Hero":"https://drive.google.com/file/d/16FcWRmTjIRyG08gp30O9Sh0ZB9RGgQx9/view",
+             "PK":"https://drive.google.com/file/d/13PqBQtLtecj6DtxiPz6ZDCb_zaXo6LI7/view",
+             "Humpty sharma ki dulhaniya":"https://drive.google.com/file/d/1DVqnwoFSlE-X-Zm04g04I3JvMI1CuJM3/view",
+             "Ishaqzaade":"https://drive.google.com/file/d/1rtuG_stgDLuhkiX1Dh0HoNK46GP08jM1/view",
+             "Yaariyan":"https://drive.google.com/file/d/1_Nz5vq9AQlVtnTlSV8uxQYVgllIuppzg/view",
+             "M.S Dhoni":"https://drive.google.com/file/d/1trVxib7DKumjkwjtvpye4_t9iUDpYNOM/view",
+             "Dhadak":"https://drive.google.com/file/d/1XLxfX_y1Djx0ge2oLinH58AM2b57-0Vn/view",
+             "Aashiqui 2":"https://drive.google.com/file/d/1LFGnKthaJXg0CWwIaW0AS3Nfo013p3Bv/view",
+             "Satyameva Jayate 2":"https://drive.google.com/file/d/1CSraXFVOtjazPidb695oAPGto0LmoCbi/view",
+             "Dhishoom":"https://drive.google.com/file/d/17Lgvja4br3NIsYCr7_wA9SpwtzB3-Ttv/view",
+             "Sui Dhaaga":"https://drive.google.com/file/d/1fHoNc9H1deSo9TFRtswwgLNsIrwQPAC5/view",
+             "Kabir Singh":"https://drive.google.com/file/d/1Oqy-vpGxXx8eyUSujgGy9tEMCfo0N3nb/view",
+             "Ramaiya Vastavaiya":"https://drive.google.com/file/d/13aBP-5FOoZqwEYd1syQAtd_YdweKy2Q5/view"
+        
+        };
+        
+        function showResults() {
+            let input = document.getElementById('searchBox').value.toLowerCase();
+            let popup = document.getElementById('popupResults');
+            popup.innerHTML = '';
+            
+            if (input.length === 0) {
+                popup.style.display = 'none';
+                return;
+            }
+            
+            let filteredItems = Object.keys(items).filter(item => item.toLowerCase().includes(input));
+            
+            if (filteredItems.length > 0) {
+                popup.style.display = 'block';
+                filteredItems.forEach(item => {
+                    let div = document.createElement('div');
+                    div.textContent = item;
+                    div.onclick = () => {
+                        window.open(items[item], '_blank');
+                    };
+                    popup.appendChild(div);
+                });
+            } else {
+                popup.style.display = 'none';
+            }
+           
+
+            function toggleMenu() {
+                var navLinks = document.getElementById("nav-links");
+                navLinks.classList.toggle("active");
+            }
+            
+            
+        }
+        
+    
