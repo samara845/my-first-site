0c6bc13bc3e488bc5b8cfa5316924155d163a910
samara845 <itz.royal.004@gmail.com>
2025-11-08 21:04:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -192,6 +192,9 @@ const items = {
              "They Call him OG" : "https://www.movieadda.fun/index33.html",
              "Kurukshetra S01" : "https://www.movieadda.fun/Kurukshera%2001.html",
              "Srikanth" : "https://www.movieadda.fun/Srikanth.html",
+             "Bahubali epic" : "https://www.movieadda.fun/BahubaliTheEpic.html",
+             "Idli kadhaai" : "https://drive.google.com/file/d/1OC2J4bTc8Ig1RWH3zV9bz3GE3XC3co7e/view",
+             "Taj Story" : "https://www.movieadda.fun/Taj%20Story.html",
 
         };
         
